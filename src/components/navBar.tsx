0776d44bdcf32fa67627b7814e70328956646dd6
Felipe Santos de Amorim <255582023@eniac.edu.br>
2026-02-11 11:18:24 -0300

    icon navbar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -41,6 +41,30 @@ export function NavBar(){
 
 					<li className="hover:bg-gray-100">
 						<a
+							href="/table"
+							className="h-16 px-6 flex justify-center items-center w-full
+							focus:text-[#2170B3]">
+							<svg
+								className="h-5 w-5"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								strokeWidth={2}
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							>
+								<rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+								<line x1="3" y1="9" x2="21" y2="9" />
+								<line x1="3" y1="15" x2="21" y2="15" />
+								<line x1="9" y1="3" x2="9" y2="21" />
+								<line x1="15" y1="3" x2="15" y2="21" />
+							</svg>
+						</a>
+					</li>
+
+					<li className="hover:bg-gray-100">
+						<a
 							href="."
 							className="h-16 px-6 flex justify-center items-center w-full
 							focus:text-[#2170B3]">
@@ -61,29 +85,6 @@ export function NavBar(){
 								<path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z">
 								
 								</path>
-							</svg>
-						</a>
-					</li>
-
-					<li className="hover:bg-gray-100">
-						<a
-							href="/table"
-							className="h-16 px-6 flex justify-center items-center w-full
-							focus:text-[#2170B3]">
-							<svg
-								className="h-5 w-5"
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								stroke-width="2"
-								stroke-linecap="round"
-								stroke-linejoin="round">
-								<path
-									d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-								<path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
 							</svg>
 						</a>
 					</li>
--- src/services/tableService.ts
+++ src/services/tableService.ts
@@ -5,7 +5,6 @@ export class TableService{
 
     async getTable(){
         const response = await API.get(`/table/${"extract_estrela_preco"}`)
-        console.log(response)
         return response.data
     }
 }
